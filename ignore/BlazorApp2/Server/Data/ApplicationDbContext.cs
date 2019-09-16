ae19516ce607b623855bb6b686aced31732f8514
LuccaBoetsDipu <52953525+LuccaBoetsDipu@users.noreply.github.com>
2019-09-16 07:48:14 +0200

    Added blazor database test project

---
+++ ignore/BlazorApp2/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    // initial migration for the database
+    // Add-Migration CreateIdentitySchema -o Data/Migations
+
+    // Update database
+    // Update-Database
+
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+}
---
+++ ignore/BlazorApp2/Shared/RegisterResult.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+internal class RegisterResult : ModelStateDictionary
+{
+    public bool Successful { get; set; }
+    public object Errors { get; set; }
+}
