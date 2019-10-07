f5ca3b9ed23c1995a3fed64c304bded5b860d6a2
CrowleySupernatural <55281416+CrowleySupernatural@users.noreply.github.com>
2019-10-07 16:00:01 +0200

    Update crash.js

--- ChappieBet/Pages/Games/crash.js
+++ ChappieBet/Pages/Games/crash.js
@@ -2,17 +2,30 @@
 function randomGetal(){
 	var aftellen = 0;
 	var aftellen1 = parseFloat(aftellen);
-	var random = Math.random() * 501;
+	var random = Math.random() * 50;
 	var random1 = parseFloat(random);
 	random1 = Math.round(random1 * 100) / 100;
+	console.log(random1);
+		var x = setInterval(function() {
+			aftellen1 = Math.round((aftellen1 + 0.01) * 100) / 100;
 
-	while(aftellen1 != random1){
-		aftellen1 = aftellen1 + 0.01;
-		document.getElementById("getal").innerHTML = aftellen1;
+			if (aftellen1.toString().length - (Math.round(aftellen1)).toString().length == 3) {
+				document.getElementById("getal").innerHTML = aftellen1;
+			} else if (aftellen1.toString().length - (Math.round(aftellen1)).toString().length != 0) {
+				document.getElementById("getal").innerHTML = aftellen1 + "0";
+				console.log(aftellen1.toString().length - (Math.round(aftellen1)).toString().length);
+			} else {
+				document.getElementById("getal").innerHTML = aftellen1 + ".00";
+			}
+			if (aftellen1 == random1) {
+				console.log("STOP");
+								clearInterval(x);
+
+				}
+		},50);
 
 
-	}
-	//document.write(random1);
+	//}
 	
 
 	document.getElementById("getal").innerHTML = aftellen1;
