35dd44500a936538ca6dcd8c67845d5904b5a328
Dries Leyers <52953642+DriesLeyers@users.noreply.github.com>
2019-10-21 15:12:07 +0200

    Roulette af denk ik

--- ChappieBet/Pages/Games/roulette.js
+++ ChappieBet/Pages/Games/roulette.js
@@ -1,6 +1,6 @@
 var bedrag = document.getElementById("beschikbareBedrag");
-var
-
+var numbersWhereBettedOn = []
+var extrasWhereBettedOn = []
 function onStart()
 {
 	
@@ -23,39 +23,91 @@ function onStart()
 	}
 }
 
-function onBetNumber()
+function onBetNumber(numberWhereBetOn)
 {
-	var numbersWhereBettedOn = []
-	var isGeselecteerd = false;
+	
+    if (numbersWhereBettedOn.includes(numberWhereBetOn)) //als er nog niet is gegokt op deze nummer, voeg toe in array
+    {
+        numbersWhereBettedOn.splice(numbersWhereBettedOn.indexOf(numberWhereBetOn) , 1);
+    }else
+    {
+        numbersWhereBettedOn.push(numberWhereBetOn);
+    }
 
-	for (var i = 0; i < 37; i++)
-	{
-
-		if (document.getElementById(i).checked == true)
-		{
-            if (numbersWhereBettedOn.includes(number)) //als er nog niet is gegokt op deze nummer, voeg toe in array
-            {
-                numbersWhereBettedOn.pop(number);
-            }else
-            {
-                numbersWhereBettedOn.push(number);
-            }
-
-        }
-	}
-
+    console.log(numbersWhereBettedOn);
 
 }
 
-function onBetEvenOrUneven(){
+function onBetExtra(extraWhereBetOn)
+{
 
-	var even = document.getElementById("even").checked;
-	var uneven = document.getElementById("uneven").checked;
+	if (extrasWhereBettedOn.includes(extraWhereBetOn)) //als er nog niet is gegokt op deze nummer, voeg toe in array
+    {
+        extrasWhereBettedOn.splice(extrasWhereBettedOn.indexOf(extraWhereBetOn) , 1);
+    }else
+    {
+        extrasWhereBettedOn.push(extraWhereBetOn);
+    }
 
 }
 
 function onBet(){
 
-	startWinningWheel();
+	console.log("dazdz")
+	var Random = Math.floor(Math.random() * 36);
+	document.getElementById("gewordenGetal").innerHTML = Random;
+	document.getElementById("rouletteSlider").value = Random;
 
+	var inzet = document.getElementById("inzet").value;
+	var nieuwbedrag =(parseInt(document.getElementById("beschikbareBedrag").innerHTML));
+
+	if (extrasWhereBettedOn.includes(112) && Random > 0 && Random <= 12) 
+	{
+		nieuwbedrag += parseInt(inzet);
+	}
+
+	if (extrasWhereBettedOn.includes(212) && Random > 12 && Random <= 24) 
+	{
+		nieuwbedrag += parseInt(inzet);
+	}
+
+	if (extrasWhereBettedOn.includes(312) && Random > 24 && Random <= 36) 
+	{
+		nieuwbedrag += parseInt(inzet);
+	}
+
+	if (extrasWhereBettedOn.includes(118) && Random > 0 && Random <= 18) 
+	{
+		nieuwbedrag += parseInt(inzet);
+	}
+
+	if (extrasWhereBettedOn.includes(1936) && Random > 18 && Random <= 36) 
+	{
+		nieuwbedrag += parseInt(inzet);
+	}
+
+	if (extrasWhereBettedOn.includes(88) && (Random % 2 == 0)) 
+	{
+		nieuwbedrag += parseInt(inzet);
+	}
+
+	if (extrasWhereBettedOn.includes(1) && (Random % 2 != 0)) 
+	{
+		nieuwbedrag += parseInt(inzet);
+	}
+
+	if (extrasWhereBettedOn.includes(2) && (Random % 2 == 0)) 
+	{
+		nieuwbedrag += parseInt(inzet);
+	}
+
+	if (extrasWhereBettedOn.includes(89) && (Random % 2 != 0)) 
+	{
+		nieuwbedrag += parseInt(inzet);
+	}
+
+
+
+
+	document.getElementById("beschikbareBedrag").innerHTML = nieuwbedrag;
 }
